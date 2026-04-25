ae3b43b6a8180bb3a684514ac0cb67e7538263da
PbayonG <>
2026-04-25 18:54:26 +0000

    Tuple Addition

---
+++ section/tuple_addition/main.py
@@ -0,0 +1,7 @@
+animal_movies = ('The Lion King', 'Jurassic Park', 'Finding Nemo')
+
+# Write your code here
+new = ("Dumbo", "Zootopia")
+animal_movies +=new
+
+print("Updated animal movies:", animal_movies)
